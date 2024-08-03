8670673026425f0cae8e09940218f7620d3d9d18
Shuja-developer <khansajaullah793@gmail.com>
2024-08-03 18:03:36 +0500

    file

---
+++ HRManagment/Models/Employee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace HRManagment.Models
+{
+    public partial class Employee
+    {
+       
+        public int EmpId { get; set; }
+        [Required]
+        [DataType(DataType.Text)]
+        public string EmpName { get; set; } = null!;
+        [Required]
+       
+        public int? Age { get; set; }
+        [Required]
+        [DataType(DataType.Text)]
+        public string? Gender { get; set; }
+        [Required]
+        [DataType(DataType.Text)]
+        public string? Designation { get; set; }
+        [Required]
+        public int? Salary { get; set; }
+    }
+}
---
+++ HRManagment/Models/EmployeeDBContext.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace HRManagment.Models
+{
+    public partial class EmployeeDBContext : DbContext
+    {
+        public EmployeeDBContext()
+        {
+        }
+
+        public EmployeeDBContext(DbContextOptions<EmployeeDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Employee> Employees { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+         
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employee>(entity =>
+            {
+                entity.HasKey(e => e.EmpId)
+                    .HasName("PK__Employee__262359ABC0373CE9");
+
+                entity.ToTable("Employee");
+
+                entity.Property(e => e.EmpId).HasColumnName("Emp_Id");
+
+                entity.Property(e => e.Designation)
+                    .HasMaxLength(100)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.EmpName)
+                    .HasMaxLength(200)
+                    .HasColumnName("Emp_Name");
+
+                entity.Property(e => e.Gender)
+                    .HasMaxLength(100)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
